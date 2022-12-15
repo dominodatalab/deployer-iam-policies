49d37e7fdd62f5d3750d6a8bdf5a4f7e55b8991f
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2022-12-15 11:17:53 -0800

    Reduce whole repo to a readme

--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" { default = "" }
-variable "stage" { default = "domino" }
-variable "create_user" { default = false }
-variable "dev" { default = false } # Route53 policies for internal testing
-
